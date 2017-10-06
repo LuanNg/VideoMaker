2464379faa799646f89de3943efc06c5cbbb8a77
MysticalNobody <micronanobot.ver3@gmail.com>
2017-10-06 18:24:28 +0300

    Add autoscale, autoposition, autocrop image Solve problem with find video from command line

--- script.py
+++ script.py
@@ -3,22 +3,24 @@ import os
 import sys
 
 from PIL import Image
-from moviepy.editor import AudioFileClip, VideoFileClip, CompositeVideoClip
+from moviepy.editor import AudioFileClip, VideoFileClip, CompositeVideoClip, ImageClip
 
 # load parameters
 '''
-video (string)
-image (string)
-audio (string)
-position(top/bottom/left/right or X Y  top left corner)
-resize(float 1.0 = 100%)
-start image clip (sec)
-duration  (sec)
-OPTIONAL: crop image (left upper right lower)
+parameter /value/
+video /string/
+image /string/
+audio /string/
+position /top/bottom/left/right/ or /X Y/  top left corner or /auto R G B/ + /int/ accuracy
+resize /float/ 1.0 = 100% or /auto/ need position auto
+start image clip /sec/
+duration  /sec/
+OPTIONAL: crop image /left upper right lower/ or /left/center/right/ need position auto
 '''
 # from command line
 if len(sys.argv) > 6:
     params = sys.argv
+    del params[0]
 # or from file
 elif os.path.isfile("input.txt"):
     with open("input.txt") as file:
@@ -30,45 +32,116 @@ else:
 # check files
 try:
     check = "video"
-    VideoFileClip(params[0])
+    os.path.isfile(params[0])
     check = "image"
-    Image.open(params[1])
+    os.path.isfile(params[1])
     check = "audio"
-    AudioFileClip(params[2])
+    os.path.isfile(params[2])
 except:
     print ("Couldn't load " + check + "file")
     quit()
 
 # set params
+autoPos = False
+autoSize = False
+keyingArea = [0, 0], [0, 0], [0, 0], [0, 0]
 try:
     pos = map(int, params[3].split())
 except ValueError:
-    pos = params[3]
-size = float(params[4])
-start = int(params[5])
-duration = int(params[6])
+    if "auto" in params[3]:
+        pos = params[3]
+        pos = pos[5:len(pos)]
+        autoPos = True
+    else:
+        pos = params[3]
+try:
+    size = float(params[4])
+except ValueError:
+    if "auto" in params[4]:
+        autoSize = True
+start = float(params[5])
+duration = float(params[6])
 
 audio = AudioFileClip(params[2])
 video = VideoFileClip(params[0]).set_audio(audio)
 
+# auto positioning image
+if autoPos:
+    video.save_frame("keyingImage.jpg", t=start)
+    imageKeying = Image.open("keyingImage.jpg")
+    colorToKey = map(int, pos.split())
+    accuracy = colorToKey[3]
+    del colorToKey[3]
+    firstPixelDetected = False;
+    pixels = list(imageKeying.getdata())
+    width, height = imageKeying.size
+    rgb_im = imageKeying.convert('RGB')
+    for y in range(height):
+        if firstPixelDetected:
+            rgb = rgb_im.getpixel((keyingArea[0][0], y))
+            if (rgb[0] < colorToKey[0] + accuracy and rgb[0] > colorToKey[0] - accuracy
+                and rgb[1] < colorToKey[1] + accuracy and rgb[1] > colorToKey[1] - accuracy
+                and rgb[2] < colorToKey[2] + accuracy and rgb[2] > colorToKey[2] - accuracy):
+                keyingArea[2][0] = keyingArea[0][0]
+                keyingArea[2][1] = y
+        else:
+            for x in range(width):
+                rgb = rgb_im.getpixel((x, y))
+                if (rgb[0] < colorToKey[0] + accuracy and rgb[0] > colorToKey[0] - accuracy
+                    and rgb[1] < colorToKey[1] + accuracy and rgb[1] > colorToKey[1] - accuracy
+                    and rgb[2] < colorToKey[2] + accuracy and rgb[2] > colorToKey[2] - accuracy
+                    and not firstPixelDetected):
+                    keyingArea[0][0] = x
+                    keyingArea[0][1] = y
+                    firstPixelDetected = True
+                elif (rgb[0] < colorToKey[0] + accuracy and rgb[0] > colorToKey[0] - accuracy
+                      and rgb[1] < colorToKey[1] + accuracy and rgb[1] > colorToKey[1] - accuracy
+                      and rgb[2] < colorToKey[2] + accuracy and rgb[2] > colorToKey[2] - accuracy
+                      and firstPixelDetected):
+                    keyingArea[1][0] = x
+                    keyingArea[1][1] = y
+                else:
+                    if (firstPixelDetected
+                        and keyingArea[1][0] - keyingArea[0][0] < 100):
+                        firstPixelDetected = False
+                        keyingArea[0][0] = 0
+                        keyingArea[0][1] = 0
+                        keyingArea[1][0] = 0
+                        keyingArea[1][1] = 0
+
+    keyingArea[3][0] = keyingArea[1][0]
+    keyingArea[3][1] = keyingArea[2][1]
+    print (keyingArea)
+    pos = keyingArea[0]
+
+image = ImageClip(params[1]).set_duration(duration)
+
+# autoscale image
+if autoSize:
+    size = float(round((keyingArea[2][1] - keyingArea[0][1]), 100) / image.h)
+
+image = image.resize(size)
+image.save_frame("1.jpg", t=0)
 
 # crop image if needed
 if len(params) == 8:
-    image = Image.open(params[1])
-    area = map(int, params[7].split())
+    image = Image.open("1.jpg")
+    width, height = image.size
+    widthNeed = keyingArea[3][0] - keyingArea[0][0]
+    try:
+        area = map(int, params[7].split())
+    except ValueError:
+        if "center" in params[7]:
+            area = [0 + (width - widthNeed) / 2, 0, width - (width - widthNeed) / 2,
+                    keyingArea[3][1] - keyingArea[0][1]]
+        elif "left" in params[7]:
+            area = [0, 0, widthNeed, keyingArea[3][1] - keyingArea[0][1]]
+        elif "right" in params:
+            area = [0 + (width - widthNeed), 0, width - 1, keyingArea[3][1] - keyingArea[0][1]]
     image = image.crop(area)
     image.save(params[1])
-# image = (ImageClip(params[1]).set_duration(duration).resize(size))
+image = ImageClip(params[1]).set_duration(duration)
 
-# auto positioning image
-image = Image.open(params[1])
-imageKeying = image;
-pixels = list(imageKeying.getdata())
-width, height = imageKeying.size
-rgb_im = imageKeying.convert('RGB')
-for x in range(width):
-    for y in range(height):
-        r, g, b = rgb_im.getpixel((x, y))
 # mix video
 if isinstance(pos[0], int):
     final_clip = CompositeVideoClip([video, image.set_pos((pos[0], pos[1])).set_start(start)])
